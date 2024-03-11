98415e9203c5c3ae39bf1f8eb5b09bf8dc1892d9
nimit007 <nimitjain.163@gmail.com>
2024-03-11 15:39:45 +0530

    minor change

--- src/components/AddTransaction.vue
+++ src/components/AddTransaction.vue
@@ -2,8 +2,8 @@
     <h3>Add new transaction</h3>
     <form id="form" @submit.prevent="onSubmit">
       <div class="form-control">
-        <label for="text">Text</label>
-        <input type="text" id="text" placeholder="Enter text..." v-model="text" />
+        <label for="text">Transaction Title</label>
+        <input type="text" id="text" placeholder="Enter transaction title..." v-model="text" />
       </div>
       <div class="form-control">
         <label for="amount"
